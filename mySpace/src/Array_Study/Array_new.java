ba32a55b990ec99d433d72a745bf9a74b3bac2e0
ashish-najre <ashishnajre88@gmail.com>
2022-04-08 22:10:50 +0530

    my data

--- mySpace/src/Array_Study/Array_new.java
+++ mySpace/src/Array_Study/Array_new.java
@@ -4,14 +4,14 @@ public class Array_new {
 
 	public static void main(String[] args)
 	{
-		String Student[]=new String[5];
+		String Student[]=new String[7];
 		
 		Student[0]="Ashish";
 		Student[1]="Anjali";
 		Student[2]="Vaibhav";
 		Student[3]="Suresh";
 		Student[4]="Shantanu";
-		
+		Student[5]="Pushkar";
 		for (int i = 0; i<=2 ; i++) 
 		{
 		System.out.println(Student[i]);	
